b7a81496f0988b68138f00dcd042ab3a95da1841
Jun99uu <igun0423@naver.com>
2022-11-26 14:56:56 +0900

    add : 인덱스페이지 오브제 추가

--- horos-web/pages/index.tsx
+++ horos-web/pages/index.tsx
@@ -1,3 +1,11 @@
+import IndexLayout from "../components/Index/IndexLayout";
+import Seo from "../components/Seo";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Seo title="Home" />
+      <IndexLayout />
+    </>
+  );
 }
