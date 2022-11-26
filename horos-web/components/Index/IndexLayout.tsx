a6f52138470f6f03e921eba001ffaeadf9f75d16
Jun99uu <igun0423@naver.com>
2022-11-26 15:52:55 +0900

    add : content-box 구성

--- horos-web/components/Index/IndexLayout.tsx
+++ horos-web/components/Index/IndexLayout.tsx
@@ -1,13 +1,25 @@
 import Objet from "./Objet";
 import { useState } from "react";
 import { Stage } from "../../interfaces/indexInterface";
+import TitleBox from "./TitleBox";
+import LoginBox from "./LoginBox";
+import RegisterBox from "./RegisterBox";
 
 export default function IndexLayout() {
-  const [stage, setStage] = useState<Stage>();
+  const [stage, setStage] = useState<Stage>(Stage.Login);
   return (
     <div className="container">
       <Objet />
-      <div className="content-box"></div>
+      <div className="content-layout">
+        <TitleBox />
+        {stage === Stage.Login ? (
+          <LoginBox setStage={setStage} />
+        ) : stage === Stage.Register ? (
+          <RegisterBox />
+        ) : (
+          <></>
+        )}
+      </div>
       <style jsx>{`
         .container {
           width: 100vw;
@@ -15,7 +27,21 @@ export default function IndexLayout() {
           display: flex;
           flex-direction: row;
           align-items: center;
-          justify-content: space-between;
+          justify-content: flex-start;
+          position: relative;
+        }
+        .content-layout {
+          width: 60vw;
+          height: 100vh;
+          position: absolute;
+          top: 0px;
+          right: 0px;
+          display: flex;
+          flex-direction: column;
+          align-items: flex-start;
+          justify-content: center;
+          gap: 20px;
+          margin-left: 30vw;
         }
       `}</style>
     </div>
