0c46843b6cf33c7aefb5d9e43a01c346c2bdae66
Jun99uu <igun0423@naver.com>
2022-12-05 00:23:38 +0900

    add : 개별 워크스페이스 추가

--- horos-web/components/WorkSpace/WorkSpaceLayout.tsx
+++ horos-web/components/WorkSpace/WorkSpaceLayout.tsx
@@ -1,5 +1,15 @@
 import { useState, useRef } from "react";
+import NoItem from "./NoItem";
+import ReactPlayer from "react-player";
 
 export default function WorkSpaceLayout() {
-  return <div></div>;
+  return (
+    <div className="container">
+      {/* <NoItem /> */}
+      {/* <ReactPlayer
+        url="https://horosocular.s3.ap-northeast-1.amazonaws.com/house.mp4"
+        controls
+      /> */}
+    </div>
+  );
 }
--- horos-web/pages/workspace/[...params].tsx
+++ horos-web/pages/workspace/[...params].tsx
@@ -1,14 +1,27 @@
 import Header from "../../components/WorkSpace/Header";
 import WorkSpaceLayout from "../../components/WorkSpace/WorkSpaceLayout";
+import { useState } from "react";
+
+enum Menu {
+  AllVideo = "내 동영상",
+  Completed = "분석이 완료된 동영상",
+  Waiting = "분석 중인 동영상",
+}
 
 export default function WorkSpace() {
+  const [category, setCategory] = useState(Menu.AllVideo);
+
   return (
     <div className="container">
-      <Header />
+      <Header category={category} setCategory={setCategory} />
       <WorkSpaceLayout />
       <style jsx>{`
         .container {
           width: 100%;
+          height: 100vh;
+          position: relative;
+          overflow-y: auto;
+          overflow-x: hidden;
           display: flex;
           flex-direction: column;
           align-items: center;
